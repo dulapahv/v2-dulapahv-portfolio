8386fc20a068990b5036d198ee26e760fc52f490
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-10-13 01:08:04 +0700

    Add fade animation to navbar, add more project sections

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var myVar;
 
 function timeout() {
-  myVar = setTimeout(showPage, 3000);
+	myVar = setTimeout(showPage, 3000);
 }
 
 function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("myDiv").style.display = "block";
+	document.getElementById("loader").style.display = "none";
+	document.getElementById("myDiv").style.display = "block";
 }
