0eaffd93fd6632a98a201c2feca2e194a26ed7ec
Dulapah Vibulsanti <dulapah6@gmail.com>
2022-12-27 20:25:23 +0700

    Update ExperiencesDetail.jsx

--- src/containers/experiences_detail/ExperiencesDetail.jsx
+++ src/containers/experiences_detail/ExperiencesDetail.jsx
@@ -95,15 +95,14 @@ const ExperiencesDetail = () => {
             <div className="experiences-detail section_padding">
                 <div className="experiences-detail__content">
                     <h1 className="gradient_text">
-                        Assist in coding for friends at KMITL and other
-                        universities
+                        Teach coding to friends at KMITL and other universities
                     </h1>
                     <p>
-                        I assist in coding for friends at KMITL and other
+                        I teach coding to friends at KMITL and other
                         universities such as Chulalongkorn University, Thammasat
                         University, Mahidol University, Kasetsart University,
                         King Mongkut's University of Technology North Bangkok,
-                        etc. They all found themselves doing better in the
+                        etc. They found themselves doing better in the
                         university and performing better in the exam.
                     </p>
                 </div>
